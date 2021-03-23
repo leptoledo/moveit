edb61e1d2252272e9df2f9f9102cf49e8fb5c733
Leandro de Paula Toledo <leandrotoledo@hotmail.com.br>
2021-03-23 02:20:20 +0000

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,9 @@
-import { useState } from 'react';
-
 import '../styles/global.css';
-
-import { ChallengesProvider } from '../contexts/ChallengesContext';
-import { Countdown } from '../components/Countdown';
-import { CountdownProvider } from '../contexts/CountdownContext';
 
 function MyApp({ Component, pageProps }) {
 
   return (
-    <ChallengesProvider>
       <Component {...pageProps} />
-    </ChallengesProvider>
   )
 }
 
